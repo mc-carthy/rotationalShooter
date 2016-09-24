20d593878939dff453e7a807dcba5c0ac70e73f6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-24 11:48:58 +0100

    Rotate player to face cursor

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+	private void Update () {
+		RotateToMousePos ();
+	}
+
+	private void RotateToMousePos () {
+		Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		float angle = AngleBetweenPoints (transform.position, mouseWorldPos);
+		transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle + 90));
+	}
+
+	private float AngleBetweenPoints (Vector2 a, Vector2 b) {
+		return Mathf.Atan2 (a.y - b.y, a.x - b.x) * Mathf.Rad2Deg; 
+	}
+}
