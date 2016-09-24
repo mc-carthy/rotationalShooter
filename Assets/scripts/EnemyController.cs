5c7b60c796670df0a36255941684b62ed9d9d127
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-24 12:39:20 +0100

    Spawn enemies on random circle around origin

---
+++ Assets/scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private void Start () {
+		moveSpeed = 0.5f;
+	}
+
+	private void Update () {
+		transform.position = Vector3.Lerp (transform.position, Vector3.zero, moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	[SerializeField]
+	private EnemyController enemy;
+	private float spawnTime = 1;
+	private bool canSpawnEnemy = true;
+
+	private void Start () {
+		StartCoroutine (SpawnEnemies ());
+	}
+
+	private IEnumerator SpawnEnemies () {
+		while (canSpawnEnemy) {
+			yield return new WaitForSeconds (spawnTime);
+			SpawnEnemy ();
+		}
+	}
+
+	private void SpawnEnemy () {
+		Instantiate (
+			enemy,
+			GenerateSpawnPositon(),
+			Quaternion.identity
+		);
+	}
+
+	private Vector2 GenerateSpawnPositon (float radius = 30) {
+		Vector2 spawnPos = Random.insideUnitCircle.normalized * radius;
+		print (spawnPos);
+		return spawnPos;
+
+	}
+}
